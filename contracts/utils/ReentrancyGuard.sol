e377cfa9da77c26f1ddce591b9dc17d72a0f09a0
Liam <liam.corr@gmail.com>
2024-04-01 07:51:09 -0400

    Fix link to blog post
    Fixed incorrect markdown-style link in documentation linking to "Reentrancy After Istanbul" blog post

--- contracts/utils/ReentrancyGuard.sol
+++ contracts/utils/ReentrancyGuard.sol
@@ -17,7 +17,7 @@ pragma solidity ^0.8.20;
  *
  * TIP: If you would like to learn more about reentrancy and alternative ways
  * to protect against it, check out our blog post
- * https://blog.openzeppelin.com/reentrancy-after-istanbul/[Reentrancy After Istanbul].
+ * [Reentrancy After Istanbul](https://blog.openzeppelin.com/reentrancy-after-istanbul/).
  */
 abstract contract ReentrancyGuard {
     // Booleans are more expensive than uint256 or any type that takes up a full
